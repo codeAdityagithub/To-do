df1aa3938ed3ad311c643cf9e1787e3d456783cd
Aditya <adityathebest2019@gmail.com>
2022-11-16 15:50:13 +0530

    commit

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7(5$5(d@vb4hq-vhcsy=$^(u6hrp#mz%i42g_^jqlfk-e7&i!g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'todo-adi008.herokuapp.com']
 
 
 # Application definition
